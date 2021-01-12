fbad70fdf12c91713d6680e59f249830a4dcf97d
AdemBenJabria <adem.bj@gmail.com>
2021-01-12 13:27:43 +0100

    Models - continuation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,14 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Posts extends Model
+class Category extends Model
 {
     use HasFactory;
-
     // Disable Laravel's mass assignment protection
     protected $guarded = [];
 
-    
-
+    /**
+     * relation with many posts, one category can has many posts
+     */
+    public function posts()
+    {
+        return $this-> hasMany('App\Post');
+    }
 
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Categories extends Model
+class Post extends Model
 {
     use HasFactory;
+
     // Disable Laravel's mass assignment protection
     protected $guarded = [];
+
+    /**
+     * Relation with Category, one post belongs to a category
+     */
+    public function Category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+    
+
 
 }
