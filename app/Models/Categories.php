9597f163c956aee9510674a03583c8aebd2d4dd4
AdemBenJabria <adem.bj@gmail.com>
2021-01-12 11:30:34 +0100

    Models ( starting )

---
+++ app/Models/Categories.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Categories extends Model
+{
+    use HasFactory;
+    // Disable Laravel's mass assignment protection
+    protected $guarded = [];
+
+}
---
+++ app/Models/Posts.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posts extends Model
+{
+    use HasFactory;
+
+    // Disable Laravel's mass assignment protection
+    protected $guarded = [];
+
+    
+
+
+}
